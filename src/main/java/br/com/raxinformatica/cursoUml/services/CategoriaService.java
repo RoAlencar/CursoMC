f26a43c7e29e3d8ca10c1c2d14ba74674a6100ca
Rodrigo de Alencar <roalencarx@gmail.com>
2021-01-20 12:42:36 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/br/com/raxinformatica/cursoUml/services/CategoriaService.java
+++ src/main/java/br/com/raxinformatica/cursoUml/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.raxinformatica.cursoUml.domain.Categoria;
 import br.com.raxinformatica.cursoUml.repositories.CategoriaRepository;
+import br.com.raxinformatica.cursoUml.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		}
 }
