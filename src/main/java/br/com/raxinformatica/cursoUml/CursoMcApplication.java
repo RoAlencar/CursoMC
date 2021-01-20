267dd4789e6fcc8816ba21dc6a0269b105a9fa77
Rodrigo de Alencar <roalencarx@gmail.com>
2021-01-20 11:07:04 -0300

    Criando operacao de instaciacao

--- src/main/java/br/com/raxinformatica/cursoUml/CursoMcApplication.java
+++ src/main/java/br/com/raxinformatica/cursoUml/CursoMcApplication.java
@@ -1,13 +1,35 @@
 package br.com.raxinformatica.cursoUml;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoMcApplication {
+import br.com.raxinformatica.cursoUml.domain.Categoria;
+import br.com.raxinformatica.cursoUml.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoMcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
+	}
+	
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+	    Categoria cat2 = new Categoria(null, "Escritório");
+	    
+	    categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	    
+	   
 	}
 
 }
