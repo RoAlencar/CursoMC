1d5272d6e9d6a461d9b8ce972c13608ee4ab9e36
Rodrigo de Alencar <roalencarx@gmail.com>
2021-01-20 16:36:12 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/br/com/raxinformatica/cursoUml/services/ClienteService.java
@@ -0,0 +1,23 @@
+package br.com.raxinformatica.cursoUml.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.raxinformatica.cursoUml.domain.Cliente;
+import br.com.raxinformatica.cursoUml.repositories.ClienteRepository;
+import br.com.raxinformatica.cursoUml.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		}
+}
